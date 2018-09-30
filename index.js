37cd83624a9ec74f8994bcdc4aa7575a2e48ff65
Nate Miller <natekmiller96@gmail.com>
2018-09-30 21:54:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function first(array){
+  return [array[0], array[1]];
+};
+
+const returnLastTwoDrivers = function first(array){
+  return [array[array.length - 2], array[array.length - 1]];
+};
+
+const createFareMultiplier = function third(array){
+  return 
+}
